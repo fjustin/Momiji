f4f8dc5bea6d8219740b0b87f350f6f1e9d123e7
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-08-22 20:34:14 +0900

    pagination added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, :except => [:index,:show]
 
   def index
-    @articles = Article.all
+    @articles = Article.page(params[:page])
   end
 
   def show
