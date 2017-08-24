d77ecdbd42ec83d513835c259698f6e12b36f033
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-08-24 21:47:44 +0900

    search-box added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_search
+
+  def set_search
+    @search = Article.ransack(params[:q])
+    @search_articles = @search.result.page(params[:page])
+  end
+
 end
