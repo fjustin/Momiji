594cc891a1d74233ff0b2e87911e9132325d1328
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-09-24 02:01:49 +0900

    tag

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   root 'articles#index'
-
+  get 'tags/:tag', to: 'articles#index', as: :tag
   get 'about/show'
 end
