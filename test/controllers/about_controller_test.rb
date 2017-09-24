f742ad0600d84673814eb5df471f87b698f7ae1e
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-09-24 20:08:01 +0900

    tag and share-button

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -4,4 +4,9 @@ class AboutControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get about page" do
+    get about_show_url
+    assert_response :success
+  end
 end
