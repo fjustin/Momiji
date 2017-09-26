cbd02d327a222aac66a52ee2d9483b9c876ad364
二見大揮 <futamidaiki@futamidaiki-no-MacBook-Air.local>
2017-09-27 02:12:24 +0900

    tag

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,6 +17,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process :resize_to_limit => [700,700]
 
+  include Cloudinary::CarrierWave
+  process :resize_to_limit => [700,700]
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
