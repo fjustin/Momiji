31963081a638ab0f79477c311a21e5d0c543e50b
futamidaiki <nimi0370@gmail.com>
2017-09-29 00:12:31 +0900

    Change design top menu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,8 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process :resize_to_limit => [700,700]
 
-  include Cloudinary::CarrierWave
-  process :resize_to_limit => [700,700]
+
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
