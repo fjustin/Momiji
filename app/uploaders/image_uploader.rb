0ffef21960a10e8d6685aec1b1b628945fb35fb0
futamidaiki <nimi0370@gmail.com>
2017-10-16 09:28:53 +0900

    Remove turbolinks

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,10 +14,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
   include Cloudinary::CarrierWave
-  process :resize_to_limit => [700,700]
-
-
+    process :resize_to_limit => [700,700]
 
 
 
