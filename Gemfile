a581f929dfdb0ff578a3b005ae2146a07a51382d
futamidaiki <nimi0370@gmail.com>
2018-03-12 00:59:15 +0900

    deviseとvalidationを日本語化

--- Gemfile
+++ Gemfile
@@ -90,6 +90,8 @@ gem 'devise-bootstrap-views'
 gem 'simple_form'
 gem 'pagedown-bootstrap-rails'
 
+gem 'rails-i18n'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,14 +14,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-=begin
   include CarrierWave::RMagick
     process :resize_to_limit => [700,700]
-=end
 
+=begin
   include Cloudinary::CarrierWave
     process :resize_to_limit => [700,700]
-
+=end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
