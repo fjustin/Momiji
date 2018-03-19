bd737f466ddedfbdfcb4809306a11e0bfd1bc947
futamidaiki <nimi0370@gmail.com>
2018-03-19 22:25:37 +0900

    fix uploader code

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,14 +13,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+=begin
   include CarrierWave::RMagick
     process :resize_to_limit => [700,700]
-
-=begin
+=end
   include Cloudinary::CarrierWave
     process :resize_to_limit => [700,700]
-=end
+    
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
