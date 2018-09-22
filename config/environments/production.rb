acf9b186f843d4d4c6dc8b3f25f6e942236f2e29
futamidaiki <nimi0370@gmail.com>
2018-09-22 11:13:42 +0900

    [ADD] tracking code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  GA.tracker = "UA-106333840-1"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
