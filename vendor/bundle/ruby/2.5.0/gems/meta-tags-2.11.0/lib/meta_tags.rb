9f5242a3e4a2a6d882bbbba83af69f68958867dd
futamidaiki <nimi0370@gmail.com>
2019-01-05 00:37:18 +0900

    add vendor files

---
+++ vendor/bundle/ruby/2.5.0/gems/meta-tags-2.11.0/lib/meta_tags.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'English'
+require 'action_controller'
+require 'action_view'
+
+# MetaTags gem namespace.
+module MetaTags
+  # Returns MetaTags gem configuration.
+  #
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  # Configures MetaTags gem.
+  #
+  # @yield [Configuration] configuration object.
+  # @example
+  #
+  #   MetaTags.configure do |config|
+  #     # config.title_limit = 100
+  #   end
+  def self.configure
+    yield config
+  end
+end
+
+require 'meta_tags/version'
+
+require 'meta_tags/configuration'
+require 'meta_tags/controller_helper'
+require 'meta_tags/meta_tags_collection'
+require 'meta_tags/renderer'
+require 'meta_tags/tag'
+require 'meta_tags/content_tag'
+require 'meta_tags/text_normalizer'
+require 'meta_tags/view_helper'
+
+ActionView::Base.include MetaTags::ViewHelper
+ActionController::Base.include MetaTags::ControllerHelper
---
+++ vendor/bundle/ruby/2.5.0/gems/meta-tags-2.11.0/lib/meta_tags/version.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module MetaTags
+  # Gem version.
+  VERSION = '2.11.0'
+end
